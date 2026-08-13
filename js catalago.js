2e1258cac292da9b4a8af762d7aa22c882d81be4
sa <samara.deren@escola.pr.gov.br>
2026-08-13 07:45:43 -0300

    mm

---
+++ js catalago.js
@@ -0,0 +1,15 @@
+export default class catalogo {
+  listadeMidias;
+    this.listaDEMIdias =[];
+}
+adicionar(midia) {
+    this.listaDEMidias.push(midia);
+    thais.rezenderizar();
+}
+ logo{
+const conteiner = documt.getElementbyid("containerCatalago")
+container.innerHTML = "";
+thais.listaDeMidias.forEach(midia = > {
+
+    container.innerHTML + = midias.obterDadosHtml();
+    ;
